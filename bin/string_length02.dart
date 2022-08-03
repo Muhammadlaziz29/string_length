0bad2c3856e7eedea4f7d13986f2c74765bc49f9
Muhammadlaziz29 <muxammadlaziz29@gmail.com>
2022-08-03 12:17:25 +0500

    helo

--- bin/string_length02.dart
+++ bin/string_length02.dart
@@ -6,6 +6,20 @@
     Returns:
         True or False
      */
+
+String func(String a) {
+  int x = a.length;
+  String p = '';
+  if (x%2==0) {
+    p = 'True';
+  }
+  if (x%2!=0) {
+    p = 'False';
+  }
+  return p;
+}
+
 void main() {
   // write your code here
+  print(func('Ye'));
 }
